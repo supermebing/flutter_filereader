157fab87894ee6f75f5b84707599b65b9c2a25a2
chenrubing <chenrubing@yaodaitong.com.cn>
2020-06-06 15:44:18 +0800

    // 隐藏垂直线条

--- ios/Classes/FileReaderView.swift
+++ ios/Classes/FileReaderView.swift
@@ -9,25 +9,25 @@ import UIKit
 import WebKit
 
 class FileReaderView: NSObject,FlutterPlatformView {
-    
+
     var _webView: FileReaderWKWebView?
-    
-    
-    
-    
-    
+
+
+
+
+
     init(withFrame frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?,binaryMessenger messenger: FlutterBinaryMessenger) {
-        
+
         super.init()
-        
+
         let channel = FlutterMethodChannel.init(name: "wv.io/FileReader_\(viewId)", binaryMessenger: messenger)
-        
+
         channel.setMethodCallHandler { (call, result) in
             if call.method == "openFile" {
                 if isSupportOpen(fileType: fileType(filePath: call.arguments as? String)){
-                    
+
                     self.openFile(filePath: call.arguments as! String)
-                    
+
                     result(true)
                 }else{
                     result(false)
@@ -38,38 +38,40 @@ class FileReaderView: NSObject,FlutterPlatformView {
                 result(isSupportOpen(fileType: fileType(filePath: call.arguments as? String)))
                 return
             }
-            
+
         }
-        
+
         self._webView = FileReaderWKWebView.init(frame: frame)
-        
+        // 隐藏垂直线条
+        _webView?.scrollView.showsVerticalScrollIndicator = false
+
     }
-    
-    
+
+
     func openFile(filePath:String)  {
-        
+
         let url = URL.init(fileURLWithPath: filePath)
-        
+
         if #available(iOS 9.0, *) {
             _webView?.loadFileURL(url, allowingReadAccessTo: url)
         } else {
             let request = URLRequest.init(url: url)
             _webView?.load(request)
         }
-       
+
     }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
     func view() -> UIView {
         return _webView!
     }
-    
-  
-    
-    
+
+
+
+
 
 }
